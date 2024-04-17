9fddf9ffa294e4a6ac6bf0f010a1f7babd5cabc5
Felipe Sousa <felipejosecsousa@gmail.com>
2024-04-17 18:25:01 -0300

    feat: add entidade estabelecimento

--- ToledoCW.Services/ToledoCW.Services.Infraestructure/Entidades/Atendente.cs
+++ ToledoCW.Services/ToledoCW.Services.Infraestructure/Entidades/Atendente.cs
@@ -1,7 +1,35 @@
+using FluentValidation;
+
+
+
+
+
 namespace ToledoCW.Services.Infraestructure.Entidades;
 
 public class Atendente
 {
     public long Id { get; set; }
     public string Nome { get; set; }
+
+    public long? Estabelecimento { get; set; }
+    
+    public Estabelecimento EstabelecimentoObj { get; set; }
+
+    public bool EhValido()
+    {
+        var _validator = new AtendenteValidator();
+        var _result = _validator.Validate(this);
+        return _result.IsValid;
+    }
+
+}
+
+public class AtendenteValidator : AbstractValidator<Atendente>
+{
+    public AtendenteValidator()
+    {
+        RuleFor(x => x.Nome).NotEmpty().WithMessage("Nome é obrigatório");
+        RuleFor(x => x.Nome).MaximumLength(100).WithMessage("Nome deve ter no máximo 100 caracteres");
+        RuleFor(x => x.Estabelecimento).NotEmpty().WithMessage("Estabelecimento é obrigatório");
+    }
 }
