ca1a67428481ef69a196411f0fe353c45c6bfabf
Felipe Sousa <felipejosecsousa@gmail.com>
2024-04-17 18:25:13 -0300

    feat: add migrations

---
+++ ToledoCW.Services/ToledoCW.Services.Infraestructure/Migrations/20240417162537_initial.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using MySql.EntityFrameworkCore.Metadata;
+
+#nullable disable
+
+namespace ToledoCW.Services.Infraestructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterDatabase()
+                .Annotation("MySQL:Charset", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "atendente",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("MySQL:ValueGenerationStrategy", MySQLValueGenerationStrategy.IdentityColumn),
+                    Nome = table.Column<string>(type: "varchar(100)", maxLength: 100, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_atendente", x => x.Id);
+                })
+                .Annotation("MySQL:Charset", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "estabelecimento",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("MySQL:ValueGenerationStrategy", MySQLValueGenerationStrategy.IdentityColumn),
+                    Nome = table.Column<string>(type: "varchar(100)", maxLength: 100, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_estabelecimento", x => x.Id);
+                })
+                .Annotation("MySQL:Charset", "utf8mb4");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "atendente");
+
+            migrationBuilder.DropTable(
+                name: "estabelecimento");
+        }
+    }
+}
---
+++ ToledoCW.Services/ToledoCW.Services.Infraestructure/Migrations/20240417170845_atendente-foreignkey.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ToledoCW.Services.Infraestructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class atendenteforeignkey : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<long>(
+                name: "Estabelecimento",
+                table: "atendente",
+                type: "bigint",
+                nullable: false,
+                defaultValue: 0L);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_atendente_Estabelecimento",
+                table: "atendente",
+                column: "Estabelecimento");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_atendente_estabelecimento_Estabelecimento",
+                table: "atendente",
+                column: "Estabelecimento",
+                principalTable: "estabelecimento",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_atendente_estabelecimento_Estabelecimento",
+                table: "atendente");
+
+            migrationBuilder.DropIndex(
+                name: "IX_atendente_Estabelecimento",
+                table: "atendente");
+
+            migrationBuilder.DropColumn(
+                name: "Estabelecimento",
+                table: "atendente");
+        }
+    }
+}
